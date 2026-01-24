04dc8d124d5094eec8697c5ffd29e2fbce1ac6ce
nerdwave-nick <nick@nerdwave.io>
2026-01-24 01:28:16 +0100

    feat: add stereo fix plugin

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    Nerdwave: {
+        name: "Nerdwave",
+        id: 1039961982079619122n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
